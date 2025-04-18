42fa08cc32a83cd588ead34d3980d1a4592a3936
a01040579861 <a01040579861@gmail.com>
2025-04-18 12:48:41 +0900

    sixcommit

--- src/components/KakaoShareButton.jsx
+++ src/components/KakaoShareButton.jsx
@@ -3,23 +3,23 @@ import { Button } from "react-bootstrap";
 
 const { Kakao } = window;
 
-const KakaoShareButton = () => {
+const KakaoShareButton = ({ data }) => {
   const url = "https://mbti-renewal.netlify.app/";
   const resultURL = window.location.href;
 
   useEffect(() => {
     Kakao.cleanup();
     Kakao.init("75f447389e58660286ad1cb6fc64aeb1");
+    Kakao.isInitialized();
   }, []);
+
   const sharekakao = () => {
     Kakao.Share.sendDefault({
       objectType: "feed",
       content: {
         title: "예비집사 판별기 결과",
-        description:
-          "예비집사님이 고양이를 키운다면 잘 맞는 고양이는 먼치킨입니다.",
-        imageUrl:
-          "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
+        description: `예비집사님이 고양이를 키운다면 잘 맞는 고양이는 ${data.name}입니다.`,
+        imageUrl: `${url}${data.image}`,
         link: {
           mobileWebUrl: resultURL,
           webUrl: resultURL,
@@ -39,7 +39,7 @@ const KakaoShareButton = () => {
 
   return (
     <Button variant="warning" onClick={sharekakao}>
-      카카오 공유하기
+      카카오톡 공유하기
     </Button>
   );
 };
