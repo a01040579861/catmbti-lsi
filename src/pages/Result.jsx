a83afb9146ac6952357a4b15f2463b50a1eeda3c
a01040579861 <a01040579861@gmail.com>
2025-04-18 12:03:39 +0900

    three commit

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styled from "styled-components";
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup } from "react-bootstrap";
 import { resultdata } from "../assets/resultdata";
+import KakaoShareButton from "../components/KakaoShareButton";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -69,6 +70,14 @@ const Desc = styled.div`
   border-radius: 8px;
 `;
 
+const ButtonGroupWrapper = styled.div`
+  display: flex;
+  gap: 14px;
+  & > button[type="button"] {
+    font-size: 2rem;
+  }
+`;
+
 const Result = () => {
   const [resultData, setResultData] = useState();
 
@@ -98,9 +107,12 @@ const Result = () => {
           예비집사님과 찰떡궁합인 고양이는
           <br /> {resultData?.best}형 {resultData?.name} 입니다.
         </Desc>
-        <Button variant="warning" onClick={handleClickButton}>
-          테스트 다시 시작하기
-        </Button>
+        <ButtonGroupWrapper>
+          <Button variant="warning" onClick={handleClickButton}>
+            테스트 다시 시작하기
+          </Button>
+          <KakaoShareButton />
+        </ButtonGroupWrapper>
       </Contents>
     </Wrapper>
   );
